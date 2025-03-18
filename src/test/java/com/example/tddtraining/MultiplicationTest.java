69c78854b3e21258a1cdb3f50d8dd61805d9b726
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2025-03-18 22:15:49 +0900

    feat: $5 + $5 = $10 에 대한 테스트 케이스를 구성

--- src/test/java/com/example/tddtraining/MultiplicationTest.java
+++ src/test/java/com/example/tddtraining/MultiplicationTest.java
@@ -25,4 +25,11 @@ public class MultiplicationTest {
         Assertions.assertEquals("CHF", Money.franc(1).currency());
     }
 
+    @Test
+    void testSimpleAddition() {
+        Money sum = Money.dollar(5).plus(Money.dollar(5));
+        Assertions.assertEquals(Money.dollar(10), sum);
+
+    }
+
 }
