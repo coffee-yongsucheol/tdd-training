8a6edadad013cdbd687bede455f5715c7f364a9f
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2025-03-19 17:38:59 +0900

    feat: Hashtable 추가하여 환율을 설정하고, 환율을 얻어내기 위한 addRate 메서드 추가

--- src/test/java/com/example/tddtraining/Bank.java
+++ src/test/java/com/example/tddtraining/Bank.java
@@ -1,11 +1,16 @@
 package com.example.tddtraining;
 
+import java.util.Hashtable;
+
 public class Bank {
+
+    private Hashtable rates = new Hashtable();
+
     public Money reduce(Expression source, String to) {
         return source.reduce(this, to);
     }
-    public void addRate(String chf, String usd, int i) {
-
+    public void addRate(String from, String to, int rate) {
+        this.rates.put(new Pair(from, to) , Integer.valueOf(rate));
     }
 
     int rate(String from, String to) {
