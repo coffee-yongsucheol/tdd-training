7f3514f31f52556f5e8cb26b871e5871db808c6a
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2025-03-19 17:23:03 +0900

    feat: 2프랑을 달러로 바꾸기 위한 테스트 케이스 추가

--- src/test/java/com/example/tddtraining/MultiplicationTest.java
+++ src/test/java/com/example/tddtraining/MultiplicationTest.java
@@ -60,4 +60,12 @@ public class MultiplicationTest {
         Assertions.assertEquals(Money.dollar(1), result);
     }
 
+    @Test
+    void testReduceMoneyDifferentCurrency() {
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(Money.franc(2), "USD");
+        Assertions.assertEquals(Money.dollar(1), result);
+    }
+
 }
