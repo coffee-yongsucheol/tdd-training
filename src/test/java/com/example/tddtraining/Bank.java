9edf59802c9bc083f1ffc9b2b53106002792f8ba
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2025-03-19 16:28:22 +0900

    fix: Money도 reduce 메소드로 처리하도록 변경

--- src/test/java/com/example/tddtraining/Bank.java
+++ src/test/java/com/example/tddtraining/Bank.java
@@ -2,7 +2,9 @@ package com.example.tddtraining;
 
 public class Bank {
     public Money reduce(Expression source, String to) {
-        if (source instanceof Money) return (Money) source;
+        if (source instanceof Money)
+            return (Money) source.reduce(to);
+
         Sum sum = (Sum) source;
         return sum.reduce(to);
     }
