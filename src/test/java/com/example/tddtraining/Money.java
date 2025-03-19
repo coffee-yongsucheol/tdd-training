6fefeaa4fd9e47ba190ee7bb0f9133a8a406d5bc
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2025-03-19 20:02:28 +0900

    fix: plus 메소드를 public화

--- src/test/java/com/example/tddtraining/Money.java
+++ src/test/java/com/example/tddtraining/Money.java
@@ -36,7 +36,7 @@ public class Money implements Expression {
         return amount + " " + currency;
     }
 
-    Expression plus(Expression addend) {
+    public Expression plus(Expression addend) {
         return new Sum(this, addend);
     }
 
