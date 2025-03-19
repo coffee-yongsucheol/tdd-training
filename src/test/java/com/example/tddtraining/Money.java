623a1bfb6f21b8adcdae2d3575e9e6ef9af80227
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2025-03-19 15:45:23 +0900

    fix: Sum을 생성하도록 변경

--- src/test/java/com/example/tddtraining/Money.java
+++ src/test/java/com/example/tddtraining/Money.java
@@ -36,7 +36,7 @@ public class Money implements Expression {
         return amount + " " + currency;
     }
 
-    Expression plus(Money added) {
-        return new Money(amount + added.amount, currency);
+    Expression plus(Money addend) {
+        return new Sum(this, addend);
     }
 }
