f8cf505ea57bb1fbea5533cb45a9c7113502f31b
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2025-03-19 20:31:18 +0900

    feat: Sum.plus()에 대한 테스트 케이스 추가

--- src/test/java/com/example/tddtraining/MultiplicationTest.java
+++ src/test/java/com/example/tddtraining/MultiplicationTest.java
@@ -85,4 +85,15 @@ public class MultiplicationTest {
         Assertions.assertEquals(Money.dollar(10), result);
     }
 
+    @Test
+    void testSumPlusMoney() {
+        Expression fiveBucks = Money.dollar(5);
+        Expression tenFrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USd", 2);
+        Expression sum = new Sum(fiveBucks, tenFrancs).plus(fiveBucks);
+        Money result = bank.reduce(sum, "USD");
+        Assertions.assertEquals(Money.dollar(15), result);
+    }
+
 }
