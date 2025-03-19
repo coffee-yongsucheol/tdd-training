aab32aa951a4dcdd81471bc91e2d54703f72da31
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2025-03-19 17:29:59 +0900

    feat: rate 메서드 추가

--- src/test/java/com/example/tddtraining/Bank.java
+++ src/test/java/com/example/tddtraining/Bank.java
@@ -4,8 +4,13 @@ public class Bank {
     public Money reduce(Expression source, String to) {
         return source.reduce(this, to);
     }
-
     public void addRate(String chf, String usd, int i) {
 
+    }
+
+    int rate(String from, String to) {
+        return (from.equals("CHF") && to.equals("USD"))
+        ? 2
+        : 1;
     }
 }
