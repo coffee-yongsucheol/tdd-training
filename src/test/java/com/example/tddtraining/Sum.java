13fa41803aa5d8dac046d542876d9e1d45f0b063
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2025-03-19 15:45:35 +0900

    feat: Sum 생성자 메서드 생성

--- src/test/java/com/example/tddtraining/Sum.java
+++ src/test/java/com/example/tddtraining/Sum.java
@@ -1,6 +1,10 @@
 package com.example.tddtraining;
 
-public class Sum {
+public class Sum implements Expression {
     public Money augend;
     public Money addend;
+
+    Sum(Money augend, Money addend) {
+
+    }
 }
